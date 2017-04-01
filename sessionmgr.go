cc5051515d3a526c32a4d3b955da53ef1baf8f93
LeeTrent <Lee.Trent.1@gmail.com>
2017-04-01 16:02:03 -0500

    Encapsulates session map inside a struct, receiver arguments are pointers

--- sessionmgr.go
+++ sessionmgr.go
@@ -10,30 +10,39 @@ type Session struct {
 	SessionId, UserName string
 }
 
-type SessionMgr map[string]string // Session ID, User Name
-
-func (sm SessionMgr) GetUserName(sessionId string) string {
-	return sm[sessionId]
+type SessionMgr struct {
+	sMap map[string]string // Session ID, User Name
 }
 
-func (sm SessionMgr) GetSession(sessionId string) (Session, error) {
-	if userName, ok := sm[sessionId]; ok {
+func NewSessionMgr() *SessionMgr {
+	sm := SessionMgr{
+		sMap: make(map[string]string),
+	}
+	return &sm
+}
+
+func (sm *SessionMgr) GetUserName(sessionId string) string {
+	return sm.sMap[sessionId]
+}
+
+func (sm *SessionMgr) GetSession(sessionId string) (Session, error) {
+	if userName, ok := sm.sMap[sessionId]; ok {
 		return Session{sessionId, userName}, nil
 	}
 	errMsg := fmt.Sprintf("Session for Session ID '%s' NOT FOUND", sessionId)
 	return Session{}, errors.New(errMsg)
 }
 
-func (sm SessionMgr) RemoveSession(sessionId string) {
-	delete(sm, sessionId)
+func (sm *SessionMgr) RemoveSession(sessionId string) {
+	delete(sm.sMap, sessionId)
 }
 
-func (sm SessionMgr) CreateSession(userName string) Session {
+func (sm *SessionMgr) CreateSession(userName string) Session {
 	sessionId := uuid.NewV4().String()
-	sm[sessionId] = userName
+	sm.sMap[sessionId] = userName
 	return Session{sessionId, userName}
 }
 
-func (sm SessionMgr) CreateSessionId() string {
+func (sm *SessionMgr) CreateSessionId() string {
 	return uuid.NewV4().String()
 }
